abbff8f30058323e003963c6b2419528cc0be5ae
Pranav Gade <pranavgade20@gmail.com>
2019-12-04 21:29:15 +0530

    can actually copy data now.

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
 from selenium import webdriver
 
+buffer = []
+
 def main():
     with open("routine.txt", "r") as routine:
         url_in = routine.readline()[1:]
@@ -21,6 +23,13 @@ def main():
 def exec_routine(driver, command):
     if(command[0:1] == "g"):
         driver.get(command[2:])
+    elif(command[0:1] == "c"):
+        driver.find_element_by_xpath(command[2:]).click()
+    elif(command[0:1] == "r"):
+        buffer.append(driver.find_element_by_xpath(command[2:]).text)
+    elif(command[0:1] == "w"):
+        driver.find_element_by_xpath(command[2:]).send_keys(buffer.pop())
+
 
 if __name__ == '__main__':
     main()
